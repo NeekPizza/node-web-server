850113d92dbe0c50b2690232747313530554bd22
ReadyDoMe <unitecomrades777@gmail.com>
2019-01-31 23:29:16 -0600

    registered helpers for hbs

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const hbs = require('hbs');
 
+// Register app as express
 var app = express();
-app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
 
+// Tell hbs to use partials at direct path
+hbs.registerPartials(__dirname + '/views/partials')
+// Tell app to use hbs as view engine
+app.set('view engine', 'hbs');
+// Register hbs methods to reference in template
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+// Routes
 app.get('/', (req, res) => {
   res.render('home.hbs',{
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to my  website',
-    currentYear: new Date().getFullYear(),
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
-    currentYear: new Date().getFullYear(),
   });
 });
 
@@ -26,4 +32,5 @@ app.get('/bad', (req, res) => {
   });
 });
 
+// Run server
 app.listen(3000, () => console.log('Server running on port 3000'));
