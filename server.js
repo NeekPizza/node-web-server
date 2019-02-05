b43d7c1acb51e5438e732af9c5403d084c82caef
ReadyDoMe <unitecomrades777@gmail.com>
2019-02-04 21:57:43 -0600

    created server log middleware

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 // Register app as express
 var app = express();
 
 // Tell hbs to use partials at direct path
 hbs.registerPartials(__dirname + '/views/partials')
+
 // Tell app to use hbs as view engine
 app.set('view engine', 'hbs');
+
+// Register middleware for server log
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}; ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
 // Register hbs methods to reference in template
 hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
 hbs.registerHelper('screamIt', (text) => text.toUpperCase());
